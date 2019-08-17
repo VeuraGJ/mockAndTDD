437f53f8726377e13d890a6a37b12e5b4df9f661
VeuraGJ <yuanxiao_ru@163.com>
2019-08-17 21:00:07 +0800

    feat:implement should_return_player1_win_when_given_2H_5S_4D_8S_AH_and_9D_3S_5H_2D_KH and test pass

--- src/main/java/poker/PokerHand.java
+++ src/main/java/poker/PokerHand.java
@@ -1,18 +1,27 @@
 package poker;
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PokerHand {
 
+    private static Map<String,Integer> cardMap = new HashMap<String,Integer>(){{
+        put("T",10);
+        put("J",11);
+        put("Q",12);
+        put("K",13);
+        put("A",14);
+    }};
     public String play(List<PokerCard> player1, List<PokerCard> player2) {
         List<Integer> sortedPlayer1 = player1.stream()
-                .map(pokerCard -> Integer.parseInt(pokerCard.getNumber()))
+                .map(pokerCard -> parseCardNumber(pokerCard))
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
         List<Integer> sortedPlayer2 = player2.stream()
-                .map(pokerCard -> Integer.parseInt(pokerCard.getNumber()))
+                .map(pokerCard -> parseCardNumber(pokerCard))
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
         for(int i =0;i < sortedPlayer1.size();i++){
@@ -24,5 +33,11 @@ public class PokerHand {
             }
         }
         return "peace";
+    }
+    private Integer parseCardNumber(PokerCard pokerCard){
+        if(pokerCard.getNumber().compareTo("2")>=0 && pokerCard.getNumber().compareTo("9")<=0){
+            return Integer.parseInt(pokerCard.getNumber());
+        }
+        return cardMap.get(pokerCard.getNumber());
     }
 }
--- src/test/java/poker/PokerHandsTest.java
+++ src/test/java/poker/PokerHandsTest.java
@@ -10,13 +10,23 @@ public class PokerHandsTest {
     @Test
     public void should_return_player2_win_when_given_2H_5S_4D_8S_6H_and_9D_3S_5H_2D_7H(){
         //given
-        new PokerCard("2H");
         PokerHand pokerHand = new PokerHand();
         //when
         String acutalResult = pokerHand.play(Arrays.asList(new PokerCard("2H"),new PokerCard("5S"),new PokerCard("4D"),new PokerCard("8S"),new PokerCard("6H"))
                 ,Arrays.asList(new PokerCard("9D"),new PokerCard("3S"),new PokerCard("5H"),new PokerCard("2D"),new PokerCard("7H")));
         //then
         Assert.assertEquals("player2 win",acutalResult);
+
+    }
+    @Test
+    public void should_return_player1_win_when_given_2H_5S_4D_8S_AH_and_9D_3S_5H_2D_KH(){
+        //given
+        PokerHand pokerHand = new PokerHand();
+        //when
+        String acutalResult = pokerHand.play(Arrays.asList(new PokerCard("2H"),new PokerCard("5S"),new PokerCard("4D"),new PokerCard("8S"),new PokerCard("AH"))
+                ,Arrays.asList(new PokerCard("9D"),new PokerCard("3S"),new PokerCard("5H"),new PokerCard("2D"),new PokerCard("KH")));
+        //then
+        Assert.assertEquals("player1 win",acutalResult);
 
     }
 }
