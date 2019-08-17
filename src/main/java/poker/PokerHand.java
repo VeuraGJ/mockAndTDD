828e0037fb6b695791ed11dd70d0b40a637222c1
VeuraGJ <yuanxiao_ru@163.com>
2019-08-17 21:32:43 +0800

    feat:implement feat:implement should_return_player1_win_when_given_2H_5S_4D_8S_AH_and_9D_3S_5H_2D_KH and test pas

--- src/main/java/poker/PokerHand.java
+++ src/main/java/poker/PokerHand.java
@@ -16,14 +16,43 @@ public class PokerHand {
         put("A",14);
     }};
     public String play(List<PokerCard> player1, List<PokerCard> player2) {
-        List<Integer> sortedPlayer1 = player1.stream()
-                .map(pokerCard -> parseCardNumber(pokerCard))
-                .sorted(Comparator.reverseOrder())
-                .collect(Collectors.toList());
-        List<Integer> sortedPlayer2 = player2.stream()
-                .map(pokerCard -> parseCardNumber(pokerCard))
-                .sorted(Comparator.reverseOrder())
-                .collect(Collectors.toList());
+        Map<String,Integer> player1ToMap = new HashMap<>();
+        Map<String,Integer> player2ToMap = new HashMap<>();
+        for(PokerCard c : player1){
+            if(player1ToMap.containsKey(c.getNumber())){
+                player1ToMap.put(c.getNumber(),player1ToMap.get(c.getNumber())+1);
+            }
+            else{
+                player1ToMap.put(c.getNumber(),1);
+            }
+        }
+        for(PokerCard c : player2){
+            if(player2ToMap.containsKey(c.getNumber())){
+                player2ToMap.put(c.getNumber(),player2ToMap.get(c.getNumber())+1);
+            }
+            else {
+                player2ToMap.put(c.getNumber(),1);
+            }
+        }
+        String player1Pair = null;
+        String player2Pair = null;
+        for(String carNumber:player1ToMap.keySet()){
+            if(player1ToMap.get(carNumber) == 2){
+                player1Pair = carNumber;
+            }
+        }
+        for(String carNumber:player2ToMap.keySet()){
+            if(player2ToMap.get(carNumber) == 2){
+                player2Pair = carNumber;
+            }
+        }
+        if(player1Pair != null && player2Pair == null){
+            return "player1 win";
+        }else if(player1Pair == null && player2Pair != null){
+            return "player2 win";
+        }
+        List<Integer> sortedPlayer1 = getSortedCardList(player1);
+        List<Integer> sortedPlayer2 = getSortedCardList(player2);
         for(int i =0;i < sortedPlayer1.size();i++){
             if(sortedPlayer1.get(i) > sortedPlayer2.get(i)){
                 return "player1 win";
@@ -33,6 +62,12 @@ public class PokerHand {
             }
         }
         return "peace";
+    }
+    private List<Integer> getSortedCardList(List<PokerCard> cards){
+        return cards.stream()
+                .map(pokerCard -> parseCardNumber(pokerCard))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
     }
     private Integer parseCardNumber(PokerCard pokerCard){
         if(pokerCard.getNumber().compareTo("2")>=0 && pokerCard.getNumber().compareTo("9")<=0){
--- src/test/java/poker/PokerHandsTest.java
+++ src/test/java/poker/PokerHandsTest.java
@@ -29,4 +29,15 @@ public class PokerHandsTest {
         Assert.assertEquals("player1 win",acutalResult);
 
     }
+    @Test
+    public void should_return_player2_win_when_given_2H_5S_4D_8S_AH_and_9D_3S_3H_2D_KH(){
+        //given
+        PokerHand pokerHand = new PokerHand();
+        //when
+        String acutalResult = pokerHand.play(Arrays.asList(new PokerCard("2H"),new PokerCard("5S"),new PokerCard("4D"),new PokerCard("8S"),new PokerCard("AH"))
+                ,Arrays.asList(new PokerCard("9D"),new PokerCard("3S"),new PokerCard("3H"),new PokerCard("2D"),new PokerCard("KH")));
+        //then
+        Assert.assertEquals("player2 win",acutalResult);
+
+    }
 }
